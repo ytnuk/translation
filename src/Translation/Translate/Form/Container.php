365f622f12f78d82de16ba0d147978d297bce42f
Vít Kutný <vitkutny@icloud.com>
2015-05-20 21:23:01 +0200

    dev

--- src/Translation/Translate/Form/Container.php
+++ src/Translation/Translate/Form/Container.php
@@ -4,6 +4,7 @@ namespace Ytnuk\Translation\Translate\Form;
 
 use Nette;
 use Ytnuk;
+use Nextras;
 
 /**
  * Class Container
@@ -13,4 +14,18 @@ use Ytnuk;
 final class Container extends Ytnuk\Orm\Form\Container
 {
 
+	/**
+	 * @inheritdoc
+	 */
+	protected function addProperty(Nextras\Orm\Entity\Reflection\PropertyMetadata $property)
+	{
+		$component = parent::addProperty($property);
+		switch ($property->name) {
+			case 'translation':
+			case 'locale':
+				$component->setOption('unique', TRUE);
+		}
+
+		return $component;
+	}
 }
