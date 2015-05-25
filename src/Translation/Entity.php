9e0cf653f31ecefa455bd2ca94472357e5140cec
Vít Kutný <vitkutny@icloud.com>
2015-05-25 22:51:08 +0200

    dev

--- src/Translation/Entity.php
+++ src/Translation/Entity.php
@@ -2,15 +2,49 @@
 
 namespace Ytnuk\Translation;
 
+use Nette;
 use Nextras;
 use Ytnuk;
 
 /**
  * @property string $key
  * @property Nextras\Orm\Relationships\OneHasMany|Ytnuk\Translation\Translate\Entity[] $translations {1:m Ytnuk\Translation\Translate\Repository $translation primary}
+ * TODO:
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Page\Entity|NULL $pageContent {1:1d Ytnuk\Page\Repository $content}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity|NULL $menuTitle {1:1d Ytnuk\Menu\Repository $title}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Web\Entity|NULL $webName {1:1d Ytnuk\Web\Repository $name}
  */
 class Entity extends Ytnuk\Orm\Entity
 {
 
 	const PROPERTY_NAME = 'key';
+
+	/**
+	 * @var Nette\Localization\ITranslator
+	 */
+	private $translator;
+
+	/**
+	 * @param Nette\Localization\ITranslator $translator
+	 */
+	public function injectTranslator(Nette\Localization\ITranslator $translator)
+	{
+		$this->translator = $translator;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function __toString()
+	{
+		$translations = $this->translations->get();
+		$translate = NULL;
+		if (count($translations) && ! $translate = $translations->findBy(['this->locale->identifier' => $locale = $this->translator->getLocale()])->fetch()) {
+			if ($length = strpos($locale, '_')) {
+				$translate = $translations->findBy(['this->locale->identifier' => substr($locale, 0, $length)])->fetch();
+			}
+		}
+
+		return $translate === NULL ? $this->translator->translate(parent::__toString()) : (string) $translate;
+	}
 }
--- src/Translation/Form/Container.php
+++ src/Translation/Form/Container.php
@@ -4,6 +4,7 @@ namespace Ytnuk\Translation\Form;
 
 use Nette;
 use Ytnuk;
+use Nextras;
 
 /**
  * Class Container
@@ -13,4 +14,39 @@ use Ytnuk;
 final class Container extends Ytnuk\Orm\Form\Container
 {
 
+	/**
+	 * @inheritdoc
+	 */
+	public function setValues($values, $erase = FALSE)
+	{
+		if ($container = $this->lookup(Ytnuk\Orm\Form\Container::class, FALSE)) {
+			$this->entity->setValue('key', $container->prefix($this->getName()));
+		}
+
+		return parent::setValues($values, $erase);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function addPropertyTranslations(Nextras\Orm\Entity\Reflection\PropertyMetadata $property)
+	{
+		$translations = parent::addPropertyOneHasMany($property);
+		if ($container = $this->lookup(Ytnuk\Orm\Form\Container::class, FALSE)) {
+			$translations->getCurrentGroup()->setOption('label', $container->formatPropertyLabel($container->getMetadata()->getProperty($this->getName())));
+		}
+
+		return $translations;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function attached($form)
+	{
+		parent::attached($form);
+		if ($container = $this->lookup(Ytnuk\Orm\Form\Container::class, FALSE)) {
+			unset($this['key']);
+		}
+	}
 }
