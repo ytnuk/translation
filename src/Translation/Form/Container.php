7970ba61bcd568039923204fd2353f1a36d8e37b
Vít Kutný <vitkutny@icloud.com>
2015-06-19 21:38:29 +0200

    dev

--- src/Translation/Form/Container.php
+++ src/Translation/Form/Container.php
@@ -19,11 +19,21 @@ final class Container extends Ytnuk\Orm\Form\Container
 	 */
 	protected function addPropertyTranslates(Nextras\Orm\Entity\Reflection\PropertyMetadata $property)
 	{
-		$translates = parent::addPropertyOneHasMany($property, 1);
+		$parent = $this->lookup(Ytnuk\Orm\Form\Container::class, FALSE);
+		$parentProperty = $parent ? $parent->getMetadata()->getProperty($this->name) : NULL;
+		$translates = parent::addPropertyOneHasMany($property, (int) ! $isNullable = $parentProperty && $parentProperty->relationshipType === Nextras\Orm\Entity\Reflection\PropertyMetadata::RELATIONSHIP_ONE_HAS_ONE_DIRECTED && $parentProperty->isNullable);
 		if ($parent = $this->lookup(Ytnuk\Orm\Form\Container::class, FALSE)) {
 			$translates->getCurrentGroup()->setOption('label', $caption = $parent->formatPropertyLabel($parent->getMetadata()->getProperty($this->getName())));
 			foreach ($translates->containers as $container) {
 				$container['value']->caption = $caption;
+			}
+		}
+		if ($isNullable && $this->getForm()->isSubmitted()) {
+			$containers = array_filter($translates->getContainers()->getArrayCopy(), function ($container) {
+				return $container instanceof parent && ! $container['delete']->isSubmittedBy();
+			});
+			if ( ! $containers) {
+				$this->removeEntity();
 			}
 		}
 
