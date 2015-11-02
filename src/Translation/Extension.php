63a58f65c2403b56857d3c6a54d6d0566b155a17
Vít Kutný <vitkutny@icloud.com>
2015-11-02 21:33:13 +0100

    extension refactor

--- src/Translation/Extension.php
+++ src/Translation/Extension.php
@@ -7,29 +7,50 @@ use Ytnuk;
 
 final class Extension
 	extends Nette\DI\CompilerExtension
-	implements Ytnuk\Config\Provider
+	implements Ytnuk\Orm\Provider, Kdyby\Translation\DI\ITranslationProvider
 {
 
-	public function getConfigResources() : array
+	public function setCompiler(
+		Nette\DI\Compiler $compiler,
+		$name
+	) : self
+	{
+		$extension = parent::setCompiler(
+			$compiler,
+			$name
+		);
+		$compiler->addExtension(
+			'kdyby.translation',
+			new Kdyby\Translation\DI\TranslationExtension
+		);
+
+		return $extension;
+	}
+
+	public function getTranslationResources() : array
 	{
 		return [
-			Ytnuk\Orm\Extension::class => [
-				'repositories' => [
-					$this->prefix('repository') => Repository::class,
-					$this->prefix('translateRepository') => Translate\Repository::class,
-					$this->prefix('localeRepository') => Locale\Repository::class,
-				],
-			],
-			Kdyby\Translation\DI\TranslationExtension::class => [
-				'dirs' => [
-					__DIR__ . '/../../locale',
-				],
-			],
-			'services' => [
-				Control\Factory::class,
-				Form\Control\Factory::class,
+			__DIR__ . '/../../locale',
+		];
+	}
+
+	public function getOrmResources() : array
+	{
+		return [
+			'repositories' => [
+				$this->prefix('repository') => Repository::class,
+				$this->prefix('translateRepository') => Translate\Repository::class,
+				$this->prefix('localeRepository') => Locale\Repository::class,
 			],
 		];
+	}
+
+	public function loadConfiguration()
+	{
+		parent::loadConfiguration();
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('control'))->setImplement(Control\Factory::class);
+		$builder->addDefinition($this->prefix('form.control'))->setImplement(Form\Control\Factory::class);
 	}
 
 	public function beforeCompile()
